1078a1dbfacd17c699636d00328d6691b548b1b4
Filipe Bacof <filipebacof@gmail.com>
2023-05-01 16:12:28 -0300

    css navbar e footer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,18 +15,24 @@ const Navbar = () => {
       <Link to="/">Instagram Clone</Link>
       <form>
         <BsSearch />
-        <input type="text" />
+        <input type="text" placeholder="Pesquisar" />
       </form>
       <ul id="nav-links">
-        <NavLink to="/">
-          <BsHouseDoorFill />
-        </NavLink>
-        <NavLink to="/login">
-          Entrar
-        </NavLink>
-        <NavLink to="/register">
-          Cadastrar
-        </NavLink>
+        <li>
+          <NavLink to="/">
+            <BsHouseDoorFill />
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/login">
+            Entrar
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/register">
+            Cadastrar
+          </NavLink>
+        </li>
       </ul>
     </nav>
   )
