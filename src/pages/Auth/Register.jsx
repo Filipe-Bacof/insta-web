1d149a2aefb07946da9ee02b4213481a21592b17
Filipe Bacof <filipebacof@gmail.com>
2023-05-01 23:21:20 -0300

    message component

--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -5,6 +5,10 @@ import { Link } from "react-router-dom";
 
 // Hooks
 import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+// Redux
+import { register, reset } from "../../slices/authSlice";
 
 const Register = () => {
   const [name, setName] = useState("");
@@ -12,6 +16,9 @@ const Register = () => {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
 
+  const dispatch = useDispatch();
+
+  const { loading, error } = useSelector((state) => state.auth);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -24,7 +31,14 @@ const Register = () => {
     };
 
     console.log(user);
+
+    dispatch(register(user))
   };
+
+  // Clean all auth states
+  useEffect(() => {
+    dispatch(reset());
+  }, [dispatch])
 
   return (
     <div id="register">
