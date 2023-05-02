3b9b81e5a2f5649ddf8a250941e912dc4437d92b
Filipe Bacof <filipebacof@gmail.com>
2023-05-02 06:49:38 -0300

    form de login

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,8 +1,45 @@
 import "./Auth.css";
 
+// Components
+import { Link } from "react-router-dom";
+import Message from "../../components/Message";
+
+// Hooks
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+// Redux
+
+
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <div>Login</div>
+    <div id="login">
+      <h2>Instagram Clone</h2>
+      <p className="subtitle">Faça o login para ver as fotos dos seus amigos.</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="E-mail"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email || ""}
+        />
+        <input
+          type="password"
+          placeholder="Senha"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password || ""}
+        />
+        <input type="submit" value="Entrar" />
+      </form>
+      <p>Não tem uma conta? <Link to="/register">Clique aqui</Link></p>
+    </div>
   )
 }
 
