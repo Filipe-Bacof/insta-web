6f9b7c5d39a8caf9034a92610e0940c11cd7ae48
Filipe Bacof <filipebacof@gmail.com>
2023-05-04 13:49:45 -0300

    css perfil de usuários

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -137,7 +137,15 @@ const Profile = () => {
                             />
                         )}
                         {id === userAuth._id
-                        ? (<p>actions</p>)
+                        ? (
+                            <div className="actions">
+                                <Link to={`/photos/${photo._id}`}>
+                                    <BsFillEyeFill />
+                                </Link>
+                                <BsPencilFill />
+                                <BsXLg />
+                            </div>
+                        )
                         : (<Link className="btn" to={`/photos/${photo._id}`}>Ver</Link>)}
                     </div>
                 ))}
