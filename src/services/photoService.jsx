3866582a24c9d9f502ed5741caad3bfbb225ee35
Filipe Bacof <filipebacof@gmail.com>
2023-05-06 00:54:54 -0300

    service de comentários

--- src/services/photoService.jsx
+++ src/services/photoService.jsx
@@ -88,7 +88,22 @@ const like = async (id, token) => {
     } catch (error) {
         console.log(error);
     }
-}
+};
+
+// Add comment to a photo
+const comment = async (data, id, token) => {
+    const config = requestConfig("PUT", data, token);
+
+    try {
+        const res = await fetch(`${api}/photos/comment/${id}`, config)
+            .then((res) => res.json())
+            .catch((err) => err);
+
+        return res;
+    } catch (error) {
+        console.log(error);
+    }
+};
 
 const photoService = {
     publishPhoto,
@@ -97,6 +112,7 @@ const photoService = {
     updatePhoto,
     getPhoto,
     like,
+    comment,
 };
 
 export default photoService;
