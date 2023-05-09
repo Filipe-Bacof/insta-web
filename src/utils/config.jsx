5bcb0724021348a592cbd24bf6f805ec79f7ab1f
Filipe Bacof <filipebacof@gmail.com>
2023-05-09 17:23:44 -0300

    requisição para railway

--- src/utils/config.jsx
+++ src/utils/config.jsx
@@ -1,5 +1,5 @@
-export const api = "http://localhost:5000/api";
-export const uploads = "http://localhost:5000/uploads";
+export const api = "https://insta-server-production.up.railway.app/api";
+export const uploads = "https://insta-server-production.up.railway.app/uploads";
 
 export const requestConfig = (method, data, token = null, image = null) => {
     let config;
